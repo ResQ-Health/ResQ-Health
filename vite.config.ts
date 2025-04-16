b494d7b1beaff01b73c545be35920563d34fe786
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2025-04-16 08:06:55 +0100

    added cors configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,12 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 10000,
     host: "0.0.0.0", // Ensures it binds to all network interfaces
+    allowedHosts: ["resqhealth.africa", "www.resqhealth.africa", "localhost"]
   },
   preview: {
     port: process.env.PORT ? Number(process.env.PORT) : 10000,
     host: "0.0.0.0",
-    allowedHosts: ["resq-health-staging.onrender.com", ".onrender.com"]
+    allowedHosts: ["resqhealth.africa", "www.resqhealth.africa", "resq-health-staging.onrender.com", ".onrender.com"]
   },
   build: {
     outDir: 'dist', // or whatever your output directory should be
